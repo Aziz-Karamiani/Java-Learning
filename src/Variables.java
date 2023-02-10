a5844d80630f9856a984ae69c3971e763dd658a5
a.karamiani <aziz.karamiani@gmail.com>
2023-02-10 22:33:56 +0330

    Primitive Type

---
+++ src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte byteNumber = (byte) 129;
+        System.out.println(byteNumber);
+        short shortNumber = 1212;
+        System.out.println(shortNumber);
+        int number = 1;
+        System.out.println(number);
+        long longNumber = 30000000000L;
+        System.out.println(longNumber);
+    }
+}
