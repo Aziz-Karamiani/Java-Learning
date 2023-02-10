a12df008f18491147ab6d5997f04e2a1cffa991e
a.karamiani <aziz.karamiani@gmail.com>
2023-02-11 00:29:54 +0330

    Array Type

---
+++ src/Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays {
+    public static void main(String[] args) {
+        char[] myName = {'A', 'Z', 'I', 'Z', ' ', 'K', 'A', 'R', 'A', 'M', 'I', 'A', 'N', 'I'};
+        for (char c : myName) {
+            System.out.print(c);
+        }
+    }
+}
