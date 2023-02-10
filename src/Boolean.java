d2a727a752ed6bbf306c6140e88c2bbfa49fe13a
a.karamiani <aziz.karamiani@gmail.com>
2023-02-11 00:24:13 +0330

    Boolean Type

---
+++ src/Boolean.java
@@ -0,0 +1,8 @@
+public class Boolean {
+    public static void main(String[] args) {
+        boolean isActive = false;
+        boolean isEnabled = true;
+        System.out.println("Is Active? " + isActive);
+        System.out.println("Is Enabled? " + isEnabled);
+    }
+}
