8efe3759601219e86b05e3a866c2869b610c2540
a.karamiani <aziz.karamiani@gmail.com>
2023-02-11 01:39:56 +0330

    While Loop

---
+++ src/WhileLoop.java
@@ -0,0 +1,19 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        // While Loop
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+
+        System.out.println("-----------------");
+
+        // do while
+        i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i <= 100);
+    }
+}
