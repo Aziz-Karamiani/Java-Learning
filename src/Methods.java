11d8424b614cfe932d1d16d3fdc53bcff0ac4a00
a.karamiani <aziz.karamiani@gmail.com>
2023-02-11 17:44:45 +0330

    Methods

---
+++ src/Methods.java
@@ -0,0 +1,15 @@
+public class Methods {
+    public static void main(String[] args) {
+        System.out.println(sum(10 , 20));
+        System.out.println(sum(100 , 200));
+    }
+
+    /**
+     * @param x is the first number
+     * @param y is the second number
+     * @return int
+     */
+    public static int sum(int x, int y) {
+        return x + y;
+    }
+}
