40f9165ab4b790f27930b60982cc918f65b86c4b
a.karamiani <aziz.karamiani@gmail.com>
2023-02-13 00:24:34 +0330

    Class instantiation

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,17 @@ public class Main {
         System.out.println(employee.getMailingName());
         System.out.println(employee.getMailingName(false));
         System.out.println(employee.getMailingName(true));
+
+        Book[] books = new Book[5];
+        books[0] = new Book("Java", "Tod", 354);
+        books[1] = new Book("PHP", "Jon", 450);
+        books[2] = new Book("Laravel", "Jan", 554);
+        books[3] = new Book("Ruby ON Rails", "Jun", 157);
+        books[4] = new Book("Golang", "Sara", 554);
+
+        for (Book book: books) {
+            if (book != null)
+                System.out.println(book.getTitle());
+        }
     }
 }
