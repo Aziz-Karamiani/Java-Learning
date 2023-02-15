21e99dc2ce27721444b675e101d4acd7577730c0
a.karamiani <aziz.karamiani@gmail.com>
2023-02-16 01:41:00 +0330

    Loop Over ArrayList

--- src/ArrayLists.java
+++ src/ArrayLists.java
@@ -6,6 +6,17 @@ public class ArrayLists {
         arrayList.add("a");
         arrayList.add("b");
         arrayList.add("c");
+        arrayList.add("d");
+        arrayList.add("e");
+        arrayList.add("f");
         System.out.println(arrayList);
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+
+        for (Object s : arrayList) {
+            System.out.println(s);
+        }
     }
 }
