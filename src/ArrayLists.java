71e137adf7c8dd65429d18bac553c40b2a6cf97f
a.karamiani <aziz.karamiani@gmail.com>
2023-02-16 01:23:51 +0330

    ArrayList

---
+++ src/ArrayLists.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArrayLists {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("a");
+        arrayList.add("b");
+        arrayList.add("c");
+        System.out.println(arrayList);
+    }
+}
